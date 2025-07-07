c1fccfd8a361b4a95c05786a2266d7e017bdfeb7
Felipe-Rafael-Barros <gitfelipebarros@gmail.com>
2025-07-07 11:21:15 -0300

    - Criação e configuração básica do Angular - Criando e Configurando componentes

--- Aula 1 - Hello, World!/Hello_World/src/app/app.ts
+++ Aula 1 - Hello, World!/Hello_World/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component'; //Importa de onde a class component está
+import { MeuPrimeiro2 } from './meu-primeiro2/meu-primeiro2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MeuPrimeiroComponent, MeuPrimeiro2], //Tenho que importar meu Component para usalo na principal
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Aula 1 - Hello, World!/Hello_World/src/app/meu-primeiro/meu-primeiro.component.ts
+++ Aula 1 - Hello, World!/Hello_World/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+    standalone: true,
     selector: 'meu-primeiro-component',
     template: `
         <p> Meu primeiro component com Angular! </p>
     `
 })
-export class MeuPrimeiroComponent { } // Padrão
+export class MeuPrimeiroComponent { } // Padrão criado
